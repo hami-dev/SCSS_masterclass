8a696b675369a755e66c09b7ad364970f047daa7
hami-dev <rinrin.xml@gmail.com>
2020-04-06 23:45:19 +0900

    #4.14 Rodic Davidson Comment (진행중)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,9 +8,9 @@ sass.compiler = require("node-sass");
 
 const routes = {
   css: {
-    watch: "project6/src/scss/*",
-    src: "project6/src/scss/project6-2.scss",
-    dest: "project6/dist/css"
+    watch: "project7/src/scss/*",
+    src: "project7/src/scss/project7.scss",
+    dest: "project7/dist/css"
   }
 };
 
